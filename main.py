a87056a3effd1a6e15c2633412cbdf4d2b299dc7
Arpit Singh <84636859+The-x-35@users.noreply.github.com>
2021-07-10 03:47:56 +0000

    -added webserver.py file to always keep it alive with uptime robot.

---
+++ main.py
@@ -0,0 +1,19 @@
+'''
+Made by Arpit.
+
+Python bot to attend google meets.
+
+
+What it does?
+1. Joins Meet.
+2. Mutes Mic and Disables camera.
+3.Sends a message that "Pls mark me Present."
+3. Include voice recognition to check if name called and then response.
+4.You tell what more to add ¯\_ʘ‿ʘ_/¯
+
+
+Packages required:
+
+'''
+from webserver import keep_alive
+keep_alive()
