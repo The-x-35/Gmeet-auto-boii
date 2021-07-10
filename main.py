331df89d36ff6ee6b86a9a0f8ac595f62941a1e5
Arpit Singh <84636859+The-x-35@users.noreply.github.com>
2021-07-10 05:20:36 +0000

    Removed webserver.py ಠ_ಠ

--- main.py
+++ main.py
@@ -8,21 +8,21 @@ What it does?
 1. Joins Meet.
 2. Mutes Mic and Disables camera.
 3.Sends a message that "Pls mark me Present."
-3. Include voice recognition to check if name called and then response.
+3. Include voice recognition to check if name called and then response.(in future)
 4.You tell what more to add ¯\_ʘ‿ʘ_/¯
 
 
 Packages required:
+selenium
 
 '''
-#from webserver import keep_alive
-#keep_alive()
+
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.firefox.options import Options
 
 chrome_options = Options()
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
 
-driver = webdriver.Chrome(options=chrome_options)
+driver = webdriver.Firefox(options=chrome_options)
 driver.get("https://youtube.com")
